23d8e1fc98ade2f8fb41a8edde6b707f95dd0cbd
Abdiel Corro <abdielcorro@outlook.com>
2023-11-09 12:04:20 -0600

    Add project files.

---
+++ LMC_Other_InventoryData/LMC_InventoryData_Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMC_Other_InventoryData
+{
+    public class LMC_InventoryData_Model
+    {
+        public string ScaleName { get; set; }
+        public int ScaleID { get; set; }
+        public string PalletNo { get; set; }
+        public string LotNo { get; set;}
+        public bool Exported { get; set;} //null?
+        public int PalletCount { get; set;}
+        public DateTime EarliestDate { get; set;}
+        public DateTime LastInventorySync { get; set;}
+    }
+}
